55e78988dca2ca611ef85fa99577fa307a263266
DramaLam <susuga62@gmail.com>
2026-06-19 14:50:01 +0300

    add auto update

--- src/main.js
+++ src/main.js
@@ -3,63 +3,7 @@ import { subscribe } from 'valtio/vanilla';
 import {
   state, validate, i18n, initPromise,
 } from './view.js';
-import runRss from './rss.js';
-
-const updateFeeds = () => {
-  const containerFeeds = document.querySelector('div[data-type="feeds"]');
-  let ul = containerFeeds.querySelector('ul');
-
-  if (!ul) {
-    ul = document.createElement('ul');
-    ul.classList.add('list-group', 'border-0', 'rounded-0');
-    containerFeeds.append(ul);
-  }
-
-  const feedLi = document.createElement('li');
-  feedLi.classList.add('list-group-item', 'border-0');
-  const lastFeed = state.feeds[state.feeds.length - 1];
-  const feedTitle = document.createElement('h6');
-  feedTitle.classList.add('m-0');
-  feedTitle.textContent = lastFeed.title;
-  const feedDescription = document.createElement('p');
-  feedDescription.classList.add('m-0', 'small', 'text-black-50');
-  feedDescription.textContent = lastFeed.description;
-
-  feedLi.append(feedTitle, feedDescription);
-
-  ul.append(feedLi);
-};
-
-const updatePosts = () => {
-  const containerPosts = document.querySelector('div[data-type="posts"]');
-  let ul = containerPosts.querySelector('ul');
-
-  if (!ul) {
-    ul = document.createElement('ul');
-    ul.classList.add('list-group', 'border-0', 'rounded-0');
-    containerPosts.append(ul);
-  }
-
-  const lastFeed = state.feeds[state.feeds.length - 1];
-  const newPosts = state.posts.filter((post) => post.feedID === lastFeed.id);
-
-  newPosts.forEach((post) => {
-    const postLi = document.createElement('li');
-    postLi.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0');
-    const postLink = document.createElement('a');
-    postLink.textContent = post.title;
-    postLink.setAttribute('href', post.link);
-    postLink.setAttribute('target', '_blank');
-
-    const btn = document.createElement('button');
-    btn.setAttribute('type', 'button');
-    btn.classList.add('btn', 'btn-outline-primary', 'btn-sm', 'flex-shrink-0', 'ms-2');
-    btn.textContent = 'Просмотр';
-
-    postLi.append(postLink, btn);
-    ul.append(postLi);
-  });
-};
+import { loadFeed, checkUpdates } from './rss.js';
 
 const app = () => {
   const input = document.querySelector('.form-control');
@@ -80,10 +24,8 @@ const app = () => {
       const { url } = state.form;
 
       // Успех
-      return runRss(url).then(() => {
+      return loadFeed(url).then(() => {
         if (state.form.error) return null;
-        updateFeeds();
-        updatePosts();
 
         state.form.url = '';
         input.value = '';
@@ -93,6 +35,9 @@ const app = () => {
           input.classList.add('is-valid');
           messageContainer.classList.add('text-success');
           messageContainer.textContent = i18n.t('success.loaded');
+          if (state.feeds.length === 1) {
+            checkUpdates();
+          }
         });
       });
     });
@@ -109,6 +54,64 @@ const app = () => {
       messageContainer.classList.add('text-danger');
       messageContainer.textContent = i18n.t(state.form.error);
     }
+  });
+
+  subscribe(state.feeds, () => {
+    const containerFeeds = document.querySelector('div[data-type="feeds"]');
+    let ul = containerFeeds.querySelector('ul');
+
+    if (!ul) {
+      ul = document.createElement('ul');
+      ul.classList.add('list-group', 'border-0', 'rounded-0');
+      containerFeeds.append(ul);
+    }
+
+    ul.innerHTML = '';
+
+    state.feeds.forEach((feed) => {
+      const li = document.createElement('li');
+      li.classList.add('list-group-item', 'border-0');
+      const feedTitle = document.createElement('h6');
+      feedTitle.classList.add('m-0');
+      feedTitle.textContent = feed.title;
+      const feedDescription = document.createElement('p');
+      feedDescription.classList.add('m-0', 'small', 'text-black-50');
+      feedDescription.textContent = feed.description;
+
+      li.append(feedTitle, feedDescription);
+
+      ul.append(li);
+    });
+  });
+
+  subscribe(state.posts, () => {
+    const containerPosts = document.querySelector('div[data-type="posts"]');
+    let ul = containerPosts.querySelector('ul');
+
+    if (!ul) {
+      ul = document.createElement('ul');
+      ul.classList.add('list-group', 'border-0', 'rounded-0');
+      containerPosts.append(ul);
+    }
+
+    ul.innerHTML = '';
+
+    state.posts.forEach((post) => {
+      const li = document.createElement('li');
+      li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0');
+      const postLink = document.createElement('a');
+      postLink.textContent = post.title;
+      postLink.setAttribute('href', post.link);
+      postLink.setAttribute('target', '_blank');
+
+      const btn = document.createElement('button');
+      btn.setAttribute('type', 'button');
+      btn.classList.add('btn', 'btn-outline-primary', 'btn-sm', 'flex-shrink-0', 'ms-2');
+      btn.textContent = 'Просмотр';
+
+      li.append(postLink, btn);
+      ul.append(li);
+    });
   });
 };
 
